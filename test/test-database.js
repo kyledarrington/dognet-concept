1fc44c4f25126e5d5a91d46bc5cd64ad506f02dd
kyledarrington <kyle@kyledarrington.com>
2019-08-22 19:59:31 -0400

    add query test

--- test/test-database.js
+++ test/test-database.js
@@ -6,9 +6,27 @@ import pool from '../database/database.js';
 
 describe('database', () => {
     describe('connection', () => {
-        it('connects', async function () {
-            var connection = await pool.getConnection;
-            assert.isOk(connection);
+        it('connects', async () => {
+        	try{
+            	var connection = await pool.getConnection();
+        	}
+        	catch(err){
+        		var message = err.message;
+        	}
+            assert.isDefined(connection, message);
         });
+    });
+    describe('query', () =>{
+    	it('retrieves one post', async () => {
+    		var queryString = 'SELECT id FROM post LIMIT 1';
+    		try {
+    			var result = await pool.query(queryString);
+    		}
+    		catch(err){
+    			var message = err.message;
+    		}
+    		assert.isDefined(result, message);
+    		assert.isArray(result);
+    	});
     });
 });
